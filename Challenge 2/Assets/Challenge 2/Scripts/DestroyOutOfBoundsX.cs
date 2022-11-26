97e019ee519b71bc93dff9bdb66ef0ea7adc1f1d
Lara Marie Isabel A. Galvez <92436688+lrglvz@users.noreply.github.com>
2022-11-26 09:25:55 +0800

    Challenge 2
    .gitignore
    .gitattributes

---
+++ Challenge 2/Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBoundsX : MonoBehaviour
+{
+    private float leftLimit = -30; //from 30, make it -30.
+    private float bottomLimit = -5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Destroy dogs if x position less than left limit
+        if (transform.position.x < leftLimit) // from greater than, make it less than.
+        {
+            Destroy(gameObject);
+        } 
+        // Destroy balls if y position is less than bottomLimit
+        else if (transform.position.z < bottomLimit)
+        {
+            Destroy(gameObject);
+            Debug.Log("Game Over!");
+        }
+
+    }
+}
---
+++ Challenge 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float timer;
+    private float timeLimit = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Creates the time to allow player spawn a dog
+        timer += Time.deltaTime;
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space)&& timer > timeLimit)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+            //resets the timer
+            timer = 0;
+        }
+    }
+}
